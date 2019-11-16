07ac23e9d65febf162964e6db84b8fd638563eb2
viveka <viveka98@hotmail.ca>
2019-11-16 14:29:40 -0500

    Video capture opens up

--- detect.py
+++ detect.py
@@ -1,4 +1,29 @@
-import numpy as numpy
+# import the necessary packages
+# from imutils import paths
+import numpy as np
+# import argparse
+# import imutils
+# import pickle
 import cv2
+import os
+import requests
+# import scikit-learn ## matching people
+
 
 cap = cv2.VideoCapture(0)
+
+while True:
+   # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
