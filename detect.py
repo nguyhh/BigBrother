08cc6f049811e8546bdd9ed5e62020444e6744f8
Ferdousara Parvin <32084900ferdousara-parvin@users.noreply.github.com>
2019-11-16 17:04:52 -0500

    delete folders

--- detect.py
+++ detect.py
@@ -1,4 +1,25 @@
-import numpy as numpy
-import cv2
+import io
+import os
+from PIL import Image
+import requests
 
-cap = cv2.VideoCapture(0)
+# get all info
+r1 = requests.get('https://pidgey2.azurewebsites.net/yulcode/employees')
+
+# Step 1: for loop to retrieve id and name
+r1obj = r1.json()
+path = os.getcwd()
+for i in r1obj:
+    os.mkdir(str(i["id"]))
+    
+
+# Step 2: for loop to retrieve picture
+for i in r1obj:
+    id = i["id"]
+    print(id)
+    r2 = requests.get('https://pidgey2.azurewebsites.net/yulcode/employees/' + str(id) + '/photo', stream = True)
+    with io.BytesIO(r2.content) as f:
+        with Image.open(f) as img:
+            img.save(path + "/" + str(id) + "/" + str(i["name"]) + ".jpeg")
+
+
