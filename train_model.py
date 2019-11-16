a5e3768bb13d09a27a11aaa43b8b6085c90ecb35
viveka <viveka98@hotmail.ca>
2019-11-16 17:05:22 -0500

    Extract, Train and Recognize

---
+++ train_model.py
@@ -0,0 +1,39 @@
+# USAGE
+# python train_model.py --embeddings output/embeddings.pickle \
+#	--recognizer output/recognizer.pickle --le output/le.pickle
+
+# import the necessary packages
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import pickle
+
+# construct the argument parser and parse the arguments
+
+embeddingsArgs="output/embeddings.pickle"
+recognizerArgs="output/recognizer.pickle"
+labelArgs="output/le.pickle"
+
+# load the face embeddings
+print("[INFO] loading face embeddings...")
+data = pickle.loads(open(embeddingsArgs, "rb").read())
+
+# encode the labels
+print("[INFO] encoding labels...")
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# train the model used to accept the 128-d embeddings of the face and
+# then produce the actual face recognition
+print("[INFO] training model...")
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+# write the actual face recognition model to disk
+f = open(recognizerArgs, "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+
+# write the label encoder to disk
+f = open(labelArgs, "wb")
+f.write(pickle.dumps(le))
+f.close()
